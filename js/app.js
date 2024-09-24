75637b80dc1002957892d1d30d396ad62d09f6e4
jahid-abi <jahidulahsan25@gmail.com>
2024-09-24 10:13:25 +0600

    add function on history button

--- js/app.js
+++ js/app.js
@@ -1,39 +1,76 @@
 // donate noakhali
-document.getElementById('donate-btn-1').addEventListener('click',function(event){
+document.getElementById('donate-btn-1').addEventListener('click', function (event) {
     event.preventDefault()
-   const donateAmount = getInputValueById('donate-noakhali');
-   const currentAmount = getTextValueById('donate-amount');
-   const newBalance = donateAmount + currentAmount;
-   const myBalance = getMyBalance('my-balance');
-   const myNewBalance = myBalance - donateAmount;
-   document.getElementById('donate-amount').innerText = newBalance;
-   document.getElementById('donate-noakhali').value = '';
-   document.getElementById('my-balance').innerText = myNewBalance;
+    const donateAmount = getInputValueById('donate-noakhali');
+    if(isNaN(donateAmount)){
+        alert('Invalid Number')
+        return;
+    };
+    const currentAmount = getTextValueById('donate-amount');
+    const newBalance = donateAmount + currentAmount;
+    const myBalance = getMyBalance('my-balance');
+    const myNewBalance = myBalance - donateAmount;
+    if (myNewBalance < 0){
+        alert('Insufficient Balance.Please recharge your account.')
+        return;
+    };
+        document.getElementById('donate-amount').innerText = newBalance;
+    document.getElementById('donate-noakhali').value = '';
+    document.getElementById('my-balance').innerText = myNewBalance;
 })
 
 // donate feni
-document.getElementById('donate-btn-2').addEventListener('click',function(event){
+document.getElementById('donate-btn-2').addEventListener('click', function (event) {
     event.preventDefault()
-   const donateAmount = getInputValueById('donate-feni');
-   const currentAmount = getTextValueById('donate-amount-2');
-   const newBalance = donateAmount + currentAmount;
-   const myBalance = getMyBalance('my-balance');
-   const myNewBalance = myBalance - donateAmount;
-   document.getElementById('donate-amount-2').innerText = newBalance;
-   document.getElementById('donate-feni').value = '';
-   document.getElementById('my-balance').innerText = myNewBalance;
-})
+    const donateAmount = getInputValueById('donate-feni');
+    if(isNaN(donateAmount)){
+        alert('Invalid Number')
+        return;
+    };
+    const currentAmount = getTextValueById('donate-amount-2');
+    const newBalance = donateAmount + currentAmount;
+    const myBalance = getMyBalance('my-balance');
+    const myNewBalance = myBalance - donateAmount;
+    if (myNewBalance < 0){
+        alert('Insufficient Balance.Please recharge your account.')
+        return;
+    };
+    document.getElementById('donate-amount-2').innerText = newBalance;
+    document.getElementById('donate-feni').value = '';
+    document.getElementById('my-balance').innerText = myNewBalance;
+});
 
 // donate quota movement
 
-document.getElementById('donate-btn-3').addEventListener('click',function(event){
+document.getElementById('donate-btn-3').addEventListener('click', function (event) {
     event.preventDefault()
-   const donateAmount = getInputValueById('donate-quota-movement');
-   const currentAmount = getTextValueById('donate-amount-3');
-   const newBalance = donateAmount + currentAmount;
-   const myBalance = getMyBalance('my-balance');
-   const myNewBalance = myBalance - donateAmount;
-   document.getElementById('donate-amount-3').innerText = newBalance;
-   document.getElementById('donate-quota-movement').value = '';
-   document.getElementById('my-balance').innerText = myNewBalance;
+    const donateAmount = getInputValueById('donate-quota-movement');
+    if(isNaN(donateAmount)){
+        alert('Invalid Number')
+        return;
+    };
+    const currentAmount = getTextValueById('donate-amount-3');
+    const newBalance = donateAmount + currentAmount;
+    const myBalance = getMyBalance('my-balance');
+    const myNewBalance = myBalance - donateAmount;
+    if (myNewBalance < 0){
+        alert('Insufficient Balance.Please recharge your account.')
+        return;
+    };
+    document.getElementById('donate-amount-3').innerText = newBalance;
+    document.getElementById('donate-quota-movement').value = '';
+    document.getElementById('my-balance').innerText = myNewBalance;
+});
+
+// button-color
+document.getElementById('nav-history-btn').addEventListener('click',function(){
+    backgroundSet('nav-history-btn');
+    showElement('history-section');
+    // const p = document.createElement('p')
+    // p.innerHTML = `<h3>hello</h3>`
+    // document.getElementById('history-section').appendChild(p)
+})
+document.getElementById('nav-donate-btn').addEventListener('click',function(){
+    backgroundSet('nav-donate-btn');
+    showElement('main-section');
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,17 +1,29 @@
-function getInputValueById(id){
+function getInputValueById(id) {
     const inputValue = document.getElementById(id).value;
     const inputNumber = Number(inputValue);
     return inputNumber;
 }
 
-function getTextValueById(id){
+function getTextValueById(id) {
     const inputText = document.getElementById(id).innerText;
     const inputNumber = Number(inputText);
     return inputNumber;
 }
 
-function getMyBalance(id){
+function getMyBalance(id) {
     const innerText = document.getElementById(id).innerText;
     const inputNumber = Number(innerText);
     return inputNumber;
+}
+
+function backgroundSet(id) {
+    document.getElementById('nav-donate-btn').classList.remove('bg-primary');
+    document.getElementById('nav-history-btn').classList.remove('bg-primary');
+    document.getElementById(id).classList.add('bg-primary');
+}
+
+function showElement(id) {
+    document.getElementById('main-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden')
 }
